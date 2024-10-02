8f9e4a21bf19cc48366c83cd1364a1e6c08d80b8
Sterling <crypticnothings@gmail.com>
2024-10-02 14:53:20 -0400

    Create script.js
    Javascript template to build on 
    
    (Restart from scratch from lost previous site)

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple script to handle navigation scrolling
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
